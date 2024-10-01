72f0f27c60c025a8f9c196dab06e4f657aa16a33
hunyaa10 <161268128+hunyaa10@users.noreply.github.com>
2024-10-01 22:34:37 +0900

    ✨​Feat: 판매물품등록기능 구현

--- src/components/userProfile/Profile.jsx
+++ src/components/userProfile/Profile.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
+// icon
+import PhoneIcon from "../../icons/phone.svg";
+import EmailIcon from "../../icons/email.svg";
+import MaleIcon from "../../icons/male.svg";
+import FemaleIcon from "../../icons/female.svg";
+import AddressIcon from "../../icons/address.svg";
+import PencilIcon from "../../icons/pencil.svg";
+import UserIcon from "../../icons/user.svg";
+
 // style
 import {
   Wrapper,
   UserInfo,
-  Img,
   Title,
   Text,
   InfoWarpper,
@@ -15,9 +23,20 @@ import {
   InfoTextIntro,
   Label,
   Input,
+  RadioLabel,
+  RadioInput,
   InputAddress,
   InputIntro,
   ModifyBtn,
+  TextBox,
+  Icon,
+  UserImg,
+  UserInfoBox,
+  GenderIcon,
+  ProfileImg,
+  Image,
+  PreviewImg,
+  InfoBoxImg,
 } from "../../styles/profileStyle";
 
 const Profile = () => {
@@ -27,8 +46,11 @@ const Profile = () => {
     email: "sldkfjsdf@naver.com",
     address: "주소입니다",
     intro: "소개글입니다",
+    gender: "male",
+    profileImg: UserIcon,
   });
   const [inputValues, setInputValues] = useState({ ...userInfo });
+  const [previewImg, setPreviewImg] = useState(UserIcon);
 
   const changeInputValue = (e) => {
     const { id, value } = e.target;
@@ -38,23 +60,79 @@ const Profile = () => {
     }));
   };
 
+  // 수정버튼클릭
   const handleModify = () => {
     setUserInfo(inputValues);
   };
 
+  // 성별변환
+  const handleGenderChange = (e) => {
+    setInputValues((prev) => ({
+      ...prev,
+      gender: e.target.value,
+    }));
+  };
+
+  // 프로필사진변환
+  const handleImgChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setInputValues((prev) => ({
+          ...prev,
+          profileImg: reader.result,
+        }));
+        setPreviewImg(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <Wrapper>
-      <UserInfo>
-        <Img>img</Img>
-        <Title>{userInfo.name}님, 환영합니다</Title>
-        <Text>{userInfo.phone}</Text>
-        <Text>{userInfo.email}</Text>
-        <Text>{userInfo.address}</Text>
-        <Text>{userInfo.intro}</Text>
-      </UserInfo>
+      <UserInfoBox>
+        <UserImg>
+          <ProfileImg>
+            <Image src={userInfo.profileImg} alt="프로필사진" />
+          </ProfileImg>
+          <Title>{userInfo.name}님, 환영합니다</Title>
+        </UserImg>
+        <UserInfo>
+          <TextBox>
+            <Icon src={PhoneIcon} alt="phone-icon" />
+            <Text>{userInfo.phone}</Text>
+          </TextBox>
+          <TextBox>
+            <Icon src={EmailIcon} alt="email-icon" />
+            <Text>{userInfo.email}</Text>
+          </TextBox>
+          <TextBox>
+            <GenderIcon
+              src={userInfo.gender === "male" ? MaleIcon : FemaleIcon}
+            />
+            <Text>{userInfo.gender === "male" ? "남성" : "여성"}</Text>
+          </TextBox>
+          <TextBox>
+            <Icon src={AddressIcon} alt="address-icon" />
+            <Text>{userInfo.address}</Text>
+          </TextBox>
+          <TextBox>
+            <Icon src={PencilIcon} alt="pencil-icon" />
+            <Text>{userInfo.intro}</Text>
+          </TextBox>
+        </UserInfo>
+      </UserInfoBox>
 
       <InfoWarpper>
         <ModiInfoBox>
+          <InfoBoxImg>
+            {previewImg && <PreviewImg src={previewImg} alt="미리보기" />}
+            <InfoBox>
+              <Label>프로필사진</Label>
+              <Input type="file" onChange={handleImgChange} />
+            </InfoBox>
+          </InfoBoxImg>
           <InfoBox>
             <InfoText>
               <Label htmlFor="name">이름</Label>
@@ -86,6 +164,29 @@ const Profile = () => {
                 value={inputValues.email}
                 onChange={changeInputValue}
               />
+            </InfoText>
+          </InfoBox>
+          <InfoBox>
+            <InfoText>
+              <RadioLabel>성별</RadioLabel>
+              <RadioLabel>
+                <RadioInput
+                  type="radio"
+                  value="male"
+                  checked={inputValues.gender === "male"}
+                  onChange={handleGenderChange}
+                />
+                남성
+              </RadioLabel>
+              <RadioLabel>
+                <RadioInput
+                  type="radio"
+                  value="female"
+                  checked={inputValues.gender === "female"}
+                  onChange={handleGenderChange}
+                />
+                여성
+              </RadioLabel>
             </InfoText>
           </InfoBox>
           <InfoBox>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,183 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import {
+  ForemostImg,
+  ForemostImgBox,
+  ImgBox,
+  InfoInput,
+  InfoInputScript,
+  InfoLabel,
+  ItemImage,
+  ItemImagesBox,
+  ItemInfo,
+  ItemInfoBox,
+  ItemInfoScript,
+  Option,
+  RegisterBtn,
+  RegisterInfo,
+  Select,
+  Warpper,
+} from "../styles/itemRegisterStyle";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    category: "",
+    size: "",
+    amount: "",
+    date: "",
+    script: "",
+  });
+  const [images, setImages] = useState([]);
+  const [foremostImg, setForemostImg] = useState(null);
+  const [successMsg, setSuccessMsg] = useState("");
+
+  // 여러장추가이미지
+  const handleImagesChange = (e) => {
+    const files = Array.from(e.target.files);
+    setImages(files);
+  };
+
+  // 대표이미지
+  const handleForemostImgChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setForemostImg(URL.createObjectURL(file));
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("상품 정보:", formData);
+    console.log("이미지 목록:", images, foremostImg);
+    setSuccessMsg("물품등록에 성공하였습니다!");
+  };
+
+  // 현재날짜
+  const today = new Date().toISOString().split("T")[0];
+
   return (
-    <>
-      <div>Register</div>
-    </>
+    <Warpper as="form" onSubmit={handleSubmit}>
+      <RegisterBtn type="submit">물품등록하기</RegisterBtn>
+      {successMsg && <p style={{ color: "red" }}>{successMsg}</p>}
+      <RegisterInfo>
+        <ItemInfoBox>
+          <ItemInfo>
+            <InfoLabel>이미지</InfoLabel>
+            <InfoInput
+              type="file"
+              id="img"
+              multiple
+              onChange={handleImagesChange}
+            />
+            {images.length > 0 && (
+              <ItemImagesBox>
+                {images.map((file, idx) => (
+                  <ItemImage
+                    key={idx}
+                    src={URL.createObjectURL(file)}
+                    alt={`이미지-${idx + 1}`}
+                  />
+                ))}
+              </ItemImagesBox>
+            )}
+          </ItemInfo>
+          <ItemInfo>
+            <InfoLabel>상품명</InfoLabel>
+            <InfoInput
+              type="text"
+              id="name"
+              placeholder="상품명을 입력하세요"
+              onChange={handleInputChange}
+              required
+            />
+          </ItemInfo>
+          <ItemInfo>
+            <InfoLabel>가격</InfoLabel>
+            <InfoInput
+              type="number"
+              id="price"
+              placeholder="990원 이상부터 판매가능"
+              min="990"
+              onChange={handleInputChange}
+              required
+            />
+            원
+          </ItemInfo>
+          <ItemInfo>
+            <InfoLabel>카테고리</InfoLabel>
+            <Select
+              name="category"
+              id="category"
+              onChange={handleInputChange}
+              required
+            >
+              <Option value="">--카테고리--</Option>
+              <Option value="male">남성의류</Option>
+              <Option value="female">여성의류</Option>
+              <Option value="child">아동의류</Option>
+            </Select>
+          </ItemInfo>
+          <ItemInfo>
+            <InfoLabel>사이즈</InfoLabel>
+            <Select name="size" id="size" onChange={handleInputChange} required>
+              <Option value="">--사이즈--</Option>
+              <Option value="small">S</Option>
+              <Option value="medium">M</Option>
+              <Option value="large">L</Option>
+            </Select>
+          </ItemInfo>
+          <ItemInfo>
+            <InfoLabel>판매수량</InfoLabel>
+            <InfoInput
+              type="number"
+              id="amount"
+              placeholder="1개 이상부터 등록가능"
+              min="1"
+              onChange={handleInputChange}
+              required
+            />
+          </ItemInfo>
+          <ItemInfo>
+            <InfoLabel>판매기간</InfoLabel>
+            <InfoInput
+              type="date"
+              id="date"
+              min={today}
+              onChange={handleInputChange}
+              required
+            />
+          </ItemInfo>
+          <ItemInfoScript>
+            <InfoLabel>상품설명</InfoLabel>
+            <InfoInputScript
+              as="textarea"
+              id="script"
+              placeholder="최소 10자이상 작성하세요."
+              onChange={handleInputChange}
+              required
+            />
+          </ItemInfoScript>
+        </ItemInfoBox>
+        <ForemostImgBox>
+          {foremostImg ? (
+            <ForemostImg src={foremostImg} alt="대표이미지" />
+          ) : (
+            <ImgBox>대표이미지를 등록하세요</ImgBox>
+          )}
+          <InfoInput type="file" onChange={handleForemostImgChange} required />
+        </ForemostImgBox>
+      </RegisterInfo>
+    </Warpper>
   );
 };
 
