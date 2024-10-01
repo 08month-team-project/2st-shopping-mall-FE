81dce1840da22e4e3f5275fb99d81b8ae46cf973
GB <ros_e2@naver.com>
2024-10-01 13:08:47 +0900

    ✨Feat: Payment,Basket.jsx 생성

---
+++ src/pages/Basket.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Basket = () => {
+  return <div>11</div>;
+};
+
+export default Basket;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import User from "../pages/User";
-import Profile from "../components/userProfile/Profile";
-import Register from "../pages/Register";
-import Selling from "../pages/Selling";
-import Buying from "../pages/Buying";
-import MainHeader from "../components/mainHeader/MainHeader";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import User from '../pages/User';
+import Profile from '../components/userProfile/Profile';
+import Register from '../pages/Register';
+import Selling from '../pages/Selling';
+import Buying from '../pages/Buying';
+import MainHeader from '../components/mainHeader/MainHeader';
+import Basket from '../pages/Basket';
+import Payment from '../pages/Payment';
 
 const Router = () => {
   return (
@@ -20,6 +22,8 @@ const Router = () => {
           <Route path="selling" element={<Selling />} />
           <Route path="buying" element={<Buying />} />
         </Route>
+        <Route path="basket" element={<Basket />} />
+        <Route path="payment" element={<Payment />} />
       </Routes>
     </BrowserRouter>
   );
