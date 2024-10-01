7b99c9ca09b12cff9224e36e791f2427c708483e
Gnar-dev <initiating94@gmail.com>
2024-10-01 12:52:38 +0900

    🚑 자잘한 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import Router from "./shared/Router";
 import { Provider } from "react-redux";
 import store from "./redux/config/ConfigStore";
-import GlobalStyle from "./utils/styles/GlobalStyle";
+import GlobalStyle from "./styles/GlobalStyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,7 +19,6 @@ function App() {
         <GlobalStyle />
         <Router />
       </Provider>
-      <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
   );
 }
