61707e3ff5af94cf69a3900c54167c8ed2d17af7
hunyaa10 <161268128+hunyaa10@users.noreply.github.com>
2024-10-01 10:45:57 +0900

    route test

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -2,13 +2,24 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import User from "../pages/User";
+import Profile from "../components/userProfile/Profile";
+import Register from "../pages/Register";
+import Selling from "../pages/Selling";
+import Buying from "../pages/Buying";
+import MainHeader from "../components/mainHeader/MainHeader";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <MainHeader />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="user" element={<User />} />
+        <Route path="user" element={<User />}>
+          <Route index element={<Profile />} />
+          <Route path="register" element={<Register />} />
+          <Route path="selling" element={<Selling />} />
+          <Route path="buying" element={<Buying />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
