5a33811e1aa3f88c55b08a08d9775edb0f32516b
ines012 <celeste07102@gmail.com>
2024-10-02 09:40:08 +0800

    ✨Feat : 회원가입 기본 폼 구성

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,21 @@
+import Input from "../components/input/Input";
+
 const Signup = () => {
   return (
     <div>
-      <p>Signup</p>
+      <h2>회원가입</h2>
+      <form>
+        <Input label="이름" type="text" name="name" />
+        <Input label="닉네임" type="text" name="nickname" />
+        <Input label="이메일" type="email" name="email" />
+        <Input label="비밀번호" type="password" name="password" />
+        <Input label="비밀번호 확인" type="password" name="confirmPassword" />
+        <Input label="전화번호" type="number" name="phone_number" />
+        <Input label="주소" type="text" name="address" />
+        <button>주소찾기</button>
+        <br />
+        <button type="submit">가입</button>
+      </form>
     </div>
   );
 };
