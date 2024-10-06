4c9f7fb543ae43e89322f8c56fd2a06c9916fd75
Juyeon Yun <lineon152@naver.com>
2024-10-07 02:29:12 +0900

    💫캐러셀 구현

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import 
     {
         Wrapper, 
-        Name, 
         Image,
         Date, 
         ImageContainer, 
@@ -10,43 +15,130 @@ import
         Icon,
         IconImage,
         Product,
+        ProductInfo,
         ProductName,
         ProductPrice,
         ProductSize,
+        ProductCheck,
+        ProductCheckButton,
+        DeleteButton,
+        Count,
+        Option,
+        QuantityButton,
         OptionBox,
         DeliveryText,
         Button,
+  
     } from '../styles/ProductDetailStyle';
 
 import HeartIcon from "../icons/heart.png";
 import StoreIcon from "../icons/store.png";
+import image1 from '../images/image1.jpg';
+import image2 from '../images/image2.jpg';
+import image3 from '../images/image3.jpg';
 
 const ProductDetail = () => {
+
+    const navigate = useNavigate(); 
+
+    const [images, setImages] = useState([image1, image2, image3]); 
+    const [selectedSize, setSelectedSize] = useState('');
+    const [quantity, setQuantity] = useState(1);
+
+    // 사이즈 선택 
+    const handleSizeChange = (event) => {
+        setSelectedSize(event.target.value);
+    };
+
+    // 수량 감소 (1개 이하로 내려가지 않도록 제한)
+    const decreaseQuantity = () => {
+        setQuantity((prevQuantity) => Math.max(1, prevQuantity - 1));
+    };
+
+    // 수량 증가 (10개 이상으로 올라가지 않도록 제한)
+    const increaseQuantity = () => {
+        setQuantity((prevQuantity) => Math.min(10, prevQuantity + 1));
+    };
+
+    // 선택한 정보를 삭제하는 함수
+    const handleDeleteSelection = () => {
+        setSelectedSize('');  
+        setQuantity(1);       
+    };
+
+     // 장바구니 페이지로 이동하는 함수
+    const handleGoToCart = () => {
+        navigate('/Buying'); 
+    };   
+    
+    
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    };
+
+
     return (
         <>
-            <Name>닉네임</Name>
             <Wrapper>
                 <ImageContainer>
                     <Image>
-                        이미지
+                        <Slider {...settings}>
+                            {images.map((image, index) => (
+                                <div key={index}>
+                                    <img src={image} alt={`Slide ${index}`}/>
+                                </div>
+                            ))}
+                        </Slider>
                     </Image>
-                    <Date>판매가능날짜</Date>
+                    <Date>한정판매</Date>
                 </ImageContainer>
                 <InfoContainer>
                     <Icon>
                         <IconImage src={StoreIcon} alt="StoreIcon"/>
                         <IconImage src={HeartIcon} alt="HeartIcon"/>
                     </Icon>
+
                     <Product>
-                        <ProductName>루즈핏 체크 셔츠</ProductName>
-                        <ProductPrice>34000원</ProductPrice>
-                        <ProductSize>사이즈 선택</ProductSize>
+                        <ProductName>세시토 싱글 블레이저</ProductName>
+                        <ProductPrice>89000원</ProductPrice>
+                        <ProductInfo>
+                            <ProductSize>
+                                <Option>
+                                    <select onChange={handleSizeChange}>
+                                        <option value="S">S</option>
+                                        <option value="M">M</option>
+                                        <option value="L">L</option>
+                                    </select>
+                                </Option>
+                                <QuantityButton>
+                                    <ProductCheckButton onClick={decreaseQuantity}>➖</ProductCheckButton>
+                                    <Count>{quantity}</Count>
+                                    <ProductCheckButton onClick={increaseQuantity}>➕</ProductCheckButton>
+                                </QuantityButton>
+                            </ProductSize>
+
+                            {selectedSize && (
+                            <ProductCheck>
+                                <p>루즈핏 체크 셔츠</p>
+                                <p>34000원</p>
+                                <p>사이즈: {selectedSize}</p>
+                                <p>수량: {quantity}</p>
+                                <DeleteButton onClick={handleDeleteSelection}>✖️</DeleteButton>
+                            </ProductCheck>
+                            )}
+                        </ProductInfo>
                         <DeliveryText>✔︎ 오전 10시까지 결제 완료 시 당일 발송</DeliveryText>
                         <DeliveryText>✔︎ 제주도 및 도시산간 지역은 추가 배송비 3000원</DeliveryText>
                     </Product>
                     <OptionBox>
-                        <Button>쇼핑백 담기</Button>
-                        <Button>바로 결제</Button>
+                        <Button onClick={handleGoToCart}>장바구니 추가</Button>
+                        <Button>결제 바로가기</Button>
                     </OptionBox>
                 </InfoContainer> 
             </Wrapper>
--- src/styles/ProductDetailStyle.js
+++ src/styles/ProductDetailStyle.js
@@ -1,43 +1,40 @@
 import styled from "styled-components";
 
+
+
+
 export const Wrapper = styled.div`
     display: flex;
     justify-content: space-evenly;
     width: 1136px;
     margin: 0 auto;
     margin-top: 50px;
-`;
-export const Name = styled.p`
-    text-color: gray;
-    text-align: right;
-    padding: 30px;
-    margin-right: 30px;
-`;
-export const Section = styled.div`
-    width: 575px;
-    height: 742px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    @media (min-width: 768px){flex-direction: row;}
-    margin: 40px auto;
+    flex-direction: row;
+    @media (max-width: 768px){flex-direction: column;}
 `;
 export const ImageContainer = styled.div`
     margin-right: 30px;
+    @media (max-width: 800px){margin-left: 80px;}
+`;
+
+export const Image = styled.div`
+    width: 500px;
+    height: 700px;
 `;
 export const InfoContainer = styled.div`
     display: flex;
     flex-direction: column;
-    
+    @media (max-width: 800px){margin-left: 80px;}
 `;
 export const Product = styled.div`
     margin-top: 80px;
     margin-bottom: 100px;
 `;
+
 export const Icon = styled.div`
     display: flex;
-    justify-content:flex-end;
+    justify-content: flex-end;
+    @media (max-width: 768px)
 `;
 export const IconImage = styled.img`
     width: 20px;
@@ -46,37 +43,105 @@ export const IconImage = styled.img`
 `;
 export const ProductName = styled.div`
     margin-bottom: 10px;
+    font-size: 20px;
 `;
 export const ProductPrice = styled.div`
-    margin-bottom: 10px;
+    margin-bottom: 20px;
+`;
+export const ProductInfo = styled.div`
+    margin-bottom: 100px;
 `;
 export const ProductSize = styled.div`
-    margin-bottom: 30px;
+    display: flex;
+    margin-bottom: 10px;
 `;
-export const OptionBox = styled.div`
+export const QuantityButton = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: 20px;
+`;
 
+export const ProductCheck = styled.div`
+    font-size: 10px;
+    padding-top: 10px;
+    
+`;
+export const ProductCheckButton = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    cursor: pointer;
+    width: 10px;
+    height: 15px;
+    margin-left: 35px;
+    color: black;
+    font-size: 15px;
+`;
+export const Option = styled.div`
+    font-size: 15px;
+`
+
+export const Count = styled.div`
+    width: 10px;
+    height: 15px;
+    text-align: center;
+    margin-left: 30px;
+    margin-bottom: 15px;
+    font-size: 20px;
+`;
+
+export const DeleteButton = styled.button`
+    margin-top: 10px;
+    background-color: white;
+    color: black;
+    &:hover {
+    background: cornflowerblue;
+    color: white;
+    transition: 0.5s;
+    } 
+`
+
+
+export const OptionBox = styled.div`
+    @media (max-width: 800px){margin-left: 60px;}
 `;
 export const DeliveryText = styled.div`
+    font-size: 12px;
 
 `;
 
-export const Image = styled.div`
-    width: 400px;
-    height: 600px;
-    border: 1px solid #ccc;
-`;
 export const Date = styled.div`
     color: rgb(55 65 81);
     text-align: center;
-    margin: 20px;
+    margin-top: 90px;
+    @media (max-width: 800px){text-align: left;}
+    @media (max-width: 800px){margin-top: 90px;}
+    @media (max-width: 800px){margin-left: 170px;}
 `;
 export const Text = styled.div`
     padding-bottom: 90px;
     text-align: left;
     width: 100%;
 `;
-export const Button = styled.div`
-    border: 1px solid #ccc;
-    margin: 30px;
+export const Button = styled.button`
+    width: 250px;
+    height: 40px;
+    display: flex;
+    margin: 20px;
+    font-size: 15px;
     text-align: center;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    background-color: lightgray;
+    &:hover {
+    background: cornflowerblue;
+    color: white;
+    transition: 0.5s;
+    } 
+    border-radius: 20px/ 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
 `;
